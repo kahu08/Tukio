90ce5558fe961f1f7ad488e9be866cd415306569
brianqeep <brites009@gmail.com>
2017-03-07 16:43:47 +0300

    events functionality added

--- app.rb
+++ app.rb
@@ -6,3 +6,28 @@ require('./lib/attendee')
 require('./lib/categorie')
 also_reload('lib/**/*.rb')
 require("pg")
+
+
+
+get ("/event/new") do
+  @categories=Categorie.all()
+erb(:event_form)
+end
+
+
+post("/event") do
+    @event_name = params.fetch("event_name")
+    @organization = params.fetch("organization")
+    @time = params.fetch("time")
+    @location = params.fetch("location")
+    @fee = params.fetch("fee").to_i()
+    event = Event.new({:event_name => @event_name, :organization => @organization, :time => @time, :location => @location, :fee => @fee})
+    event.save()
+    @events = Event.all()
+    erb(:events)
+end
+
+get("/events/:id") do
+  @events = Event.find(params.fetch("id").to_i())
+  erb(:event)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307075709) do
+ActiveRecord::Schema.define(version: 20170307120218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,13 @@ ActiveRecord::Schema.define(version: 20170307075709) do
   create_table "events", force: :cascade do |t|
     t.string   "organization"
     t.string   "event_name"
-    t.time     "time"
     t.string   "location"
     t.integer  "fee"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "categorie_id"
     t.integer  "attendee_id"
+    t.datetime "time"
   end
 
 end
