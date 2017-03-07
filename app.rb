d871e11d1053c74710a47fa574926151c944d44a
kahu08 <peterkahurani@gmail.com>
2017-03-07 17:17:06 +0300

    attendee form, manipulation off app.rb and displaying attendees

--- app.rb
+++ app.rb
@@ -6,3 +6,27 @@ require('./lib/attendee')
 require('./lib/categorie')
 also_reload('lib/**/*.rb')
 require("pg")
+
+
+
+#form for adding attendee to event
+get('/attendees/new') do
+  @events = Event.all()
+  erb(:attendee_form)
+end
+
+#display all attendees
+get('/attendees') do
+  @attendees = Attendee.all()
+  erb(:attendees)
+end
+
+#display the attendee on attendees
+post('/attendees') do
+  name = params.fetch("name")
+  number = params.fetch("number")
+  attendee = Attendee.new({:name => name, :number => number, :id => nil})
+  attendee.save()
+  # @attendees = Attendee.all()
+  erb(:fiti)
+end
