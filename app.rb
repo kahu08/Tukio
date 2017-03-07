ba254387c5d64c817be8624f01784080082fdeed
ndongA <boydbugle@gmai.com>
2017-03-07 15:35:10 +0300

    db alter

--- app.rb
+++ app.rb
@@ -6,3 +6,25 @@ require('./lib/attendee')
 require('./lib/categorie')
 also_reload('lib/**/*.rb')
 require("pg")
+
+
+get('/categories/new')do
+  erb(:category_form)
+end
+
+post('/categories')do
+  name = params.fetch("categorie_name")
+  category = Categorie.new({:categorie_name => name})
+  category.save()
+  erb(:success)
+end
+
+get('/categories')do
+@category = Categorie.all()
+  erb(:categories)
+end
+
+get('/categories/:id')do
+@category = Categorie.find(params.fetch("id").to_i())
+  erb(:category)
+end
